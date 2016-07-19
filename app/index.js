f0c4937cdb74c6da3779a823a009ecbbaa63cc41
MeePwn <maybewaityou@gmail.com>
2016-07-19 17:30:58 +0800

    add ReduxExample

--- app/index.js
+++ app/index.js
@@ -6,27 +6,12 @@
  */
 
 import React, { Component } from 'react';
-import styles from './styles/Main';
-import {
-  Text,
-  View
-} from 'react-native';
+import ReduxExample from './reduxExample/reduxExample';
 
 class App extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit index.ios.js
-                </Text>
-                <Text style={styles.instructions}>
-                    Press Cmd+R to reload,{'\n'}
-                    Cmd+D or shake for dev menu
-                </Text>
-            </View>
+            <ReduxExample />
         );
     }
 }
