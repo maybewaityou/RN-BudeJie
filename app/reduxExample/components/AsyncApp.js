428de6259e547f1e289b49fde70e51102b5485c0
MeePwn <maybewaityou@gmail.com>
2016-07-19 17:44:51 +0800

    add ReduxExample

--- app/reduxExample/components/AsyncApp.js
+++ app/reduxExample/components/AsyncApp.js
@@ -21,16 +21,36 @@ class AsyncApp extends Component {
         const { dispatch, calculate, calculateDelay } = this.props;
         return (
             <View style={styles.container}>
-                <TouchableHighlight onPress={() => {
-                    dispatch(add(calculate));
-                }}>
+                <View style={styles.buttonLayout}>
+                    <TouchableHighlight style={styles.addButton} onPress={() => {
+                        dispatch(add(calculate));
+                    }}>
+                        <Text>Add</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.subtractButton} onPress={() => {
+                        dispatch(subtract(calculate));
+                    }}>
+                        <Text>Subtract</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.delayAddButton} onPress={() => {
+                        dispatch(delayAdd(calculateDelay, 1000));
+                    }}>
+                        <Text>DelayAdd</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.delaySubtractButton} onPress={() => {
+                        dispatch(delaySubtract(calculateDelay, 1000));
+                    }}>
+                        <Text>DelaySubtract</Text>
+                    </TouchableHighlight>
+                </View>
+                <View>
                     <Text>
-                        Add
+                        {calculate}
                     </Text>
-                </TouchableHighlight>
-                <Text>
-                    {calculate}
-                </Text>
+                    <Text>
+                        {calculateDelay}
+                    </Text>
+                </View>
             </View>
         );
     }
--- app/styles/Main.js
+++ app/styles/Main.js
@@ -6,24 +6,29 @@
  */
 
 import {
-  StyleSheet
+    StyleSheet
 } from 'react-native';
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    buttonLayout: {
+        flexDirection: 'row',
+    },
+    addButton: {
+        flex: 1
+    },
+    subtractButton: {
+        flex: 1
+    },
+    delayAddButton: {
+        flex: 1
+    },
+    delaySubtractButton: {
+        flex: 1
+    },
 });
