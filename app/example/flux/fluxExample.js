8856cd75fedb0f37399ecfdd116f9102437e8bd9
MeePwn <maybewaityou@gmail.com>
2016-07-21 09:01:37 +0800

    add FluxExample

---
+++ app/example/flux/fluxExample.js
@@ -0,0 +1,19 @@
+/* jshint esversion: 6 */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import FluxApp from './components/FluxApp';
+
+class FluxExample extends Component {
+    render() {
+        return (
+            <FluxApp />
+        );
+    }
+}
+
+export default FluxExample;
