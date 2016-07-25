142d21fc8e801f1b54f20fda675638cfcc6a4fbd
MeePwn <maybewaityou@gmail.com>
2016-07-25 09:14:13 +0800

    add action

--- app/example/flux/actions/Actions.js
+++ app/example/flux/actions/Actions.js
@@ -1,12 +1,33 @@
 /* jshint esversion: 6*/
+'use strict';
 
 import Dispatcher from '../dispatcher/Dispatcher';
+
+/* ============================= Action Type Start =============================== */
+const DEFAULT = 'DEFAULT';
+const ADD = 'ADD';
+const SUBTRACT = 'SUBTRACT';
+const DELAY_ADD = 'DELAY_ADD';
+const DELAY_SUBTRACT = 'DELAY_SUBTRACT';
+/* ============================= Action Type End =============================== */
 
 let Actions = {
     create: (text) => {
         Dispatcher.dispatch({
             actionType: 'create',
             text: text,
+        });
+    },
+    add: (item) => {
+        Dispatcher.dispatch({
+            type: ADD,
+            item: item + 1,
+        });
+    },
+    subtract: (item) => {
+        Dispatcher.dispatch({
+            type: ADD,
+            item: item - 1,
         });
     },
     updateText: (id, text) => {
