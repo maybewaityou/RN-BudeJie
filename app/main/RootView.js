312cd417b19cf5522532336530dd66885f19e6b9
MeePwn <maybewaityou@gmail.com>
2016-07-25 15:23:10 +0800

    delete

--- app/main/RootView.js
+++ app/main/RootView.js
@@ -2,15 +2,8 @@
 
 import React, { Component } from 'react';
 import TabBar from './components/TabBar';
-import {
-    View
-} from 'react-native';
 
 class RootView extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <TabBar />
