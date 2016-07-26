e241c79a05e4b2dc0cc70066b49e0cce5d9a50b1
MeePwn <maybewaityou@gmail.com>
2016-07-26 15:21:32 +0800

    add LoginComponent

--- app/main/components/LoginComponent.js
+++ app/main/components/LoginComponent.js
@@ -2,25 +2,39 @@
 
 import React, { Component } from 'react';
 import styles from '../../styles/Main';
+import Images from '../constant/Images';
 import {
     View,
     Text,
-    Image
+    Image,
+    TouchableOpacity
 } from 'react-native';
 
 class LoginComponent extends React.Component {
     constructor(props) {
         super(props);
 
-
+        this.state = {
+            closeImage: Images.loginCloseIcon
+        };
     }
 
     render() {
         return (
-            <Image source={{uri: ''}} style={styles.container}>
-                <Text onPress={this.props.close}>
-                    dismiss
-                </Text>
+            <Image source={{uri: Images.loginViewBackgroundIcon}} style={{ flex: 1, alignItems: 'center' }}>
+                <TouchableOpacity
+                    activeOpacity={1}
+                    style={{ alignSelf: 'flex-start', marginTop: 30, marginLeft: 10, padding: 10 }}
+                    onPress={this.props.close}
+                    onPressIn={() => {
+                        this.setState({closeImage: Images.loginCloseIcon});
+                    }}
+                    onPressOut={() => {
+                        this.setState({closeImage: Images.loginCloseIcon});
+                    }}
+                >
+                    <Image source={{uri: this.state.closeImage}} style={{ width: 16, height: 16 }}/>
+                </TouchableOpacity>
             </Image>
         );
     }
