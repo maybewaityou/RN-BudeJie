28e07f523d2369a5cf59cb9b1266a49bc836bd9e
MeePwn <maybewaityou@gmail.com>
2016-07-28 11:20:58 +0800

    add MinePage ListView

--- app/main/tab/mine/container/MineContainer.js
+++ app/main/tab/mine/container/MineContainer.js
@@ -5,13 +5,16 @@ import { connect } from 'react-redux';
 import styles from '../../../../styles/Main';
 import LocalString from '../../../constant/LocalString';
 import MineView from '../view/MineView';
+import Dimensions from 'Dimensions';
+
+const { width, height } = Dimensions.get('window');
 
 class MineContainer extends React.Component {
     render() {
         const { dispatch } = this.props;
         return (
             <MineView
-                
+                width={width}
             />
         );
     }
--- app/main/tab/mine/view/MineView.js
+++ app/main/tab/mine/view/MineView.js
@@ -2,21 +2,75 @@
 
 import React from 'react';
 import styles from '../../../../styles/Main';
+import Images from '../../../constant/Images';
+import Color from '../../../constant/Color';
 import {
-    View
+    View,
+    ListView,
+    Image,
+    Text,
+    TouchableOpacity
 } from 'react-native';
 
 class MineView extends React.Component {
     constructor(props) {
         super(props);
 
+        const data = [
+            {
+                image: Images.random,
+                title: '登录/注册'
+            },
+            {
+                title: '离线下载'
+            }
+        ];
 
+        var dataSource = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.state = {
+            dataSource: dataSource.cloneWithRows(data)
+        };
+
+        this.renderRow = this.renderRow.bind(this);
+        this.renderFooter = this.renderFooter.bind(this);
+    }
+
+    renderRow(rowData, sectionID, rowID) {
+        return (
+            <View>
+                <TouchableOpacity
+                    activeOpacity={0.8}
+                    style={{ backgroundColor: 'white', flexDirection: 'row', alignItems: 'center',  width: this.props.width, height: 44, borderTopWidth: 0.5, borderBottomWidth: 0.5, borderColor: Color.defaultColor }}
+                >
+                    {
+                        rowData.image ? <Image source={{uri: rowData.image}} style={{ marginLeft: 10, width: 33, height: 33 }} /> : <View />
+                    }
+                    <Text style={[styles.commonText, { fontSize: 16, marginLeft: 10 }]}>
+                        {rowData.title}
+                    </Text>
+                </TouchableOpacity>
+                <View
+                    style={{ marginTop: 20 }}
+                />
+            </View>
+        );
+    }
+
+    renderFooter() {
+        
     }
 
     render() {
         return (
             <View style={styles.container}>
-
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderRow}
+                    renderFooter={this.renderFooter}
+                />
             </View>
         );
     }
