ca1ae92e777d9627e225a09db25b898e4b526a1d
MeePwn <maybewaityou@gmail.com>
2016-08-01 15:34:56 +0800

    delete styles

--- app/main/tab/common/cell/CellBottomView.js
+++ app/main/tab/common/cell/CellBottomView.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
-import styles from '../../../../styles/Main';
 import Images from '../../../constant/Images';
 import Button from '../../../components/Button';
 import {
+    StyleSheet,
     View,
     Text,
     Image
@@ -11,50 +11,50 @@ import {
 class CellBottomView extends Component {
     render() {
         return (
-            <View style={[{ justifyContent: 'center', alignItems: 'center', marginTop: 10, width: this.props.width }, this.props.style]}>
+            <View style={[styles.cellBottomViewLayout, { width: this.props.width }, this.props.style]}>
                 <Image source={{uri: 'cell-content-line'}} style={{ width: this.props.width, height: 1 }} />
-                <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', height: 40 }}>
+                <View style={styles.bottomViewLayout}>
                     <Button
                         leftImage='mainCellDing'
                         highlightLeftImage='mainCellDingClick'
-                        leftImageStyle={{ width: 20, height: 20 }}
+                        leftImageStyle={styles.buttonLeftImage}
                         title={this.props.data.ding ? this.props.data.ding : '顶'}
                         highlightTitleColor='red'
-                        titleStyle={{ fontSize: 14, marginLeft: 5 }}
-                        style={{ flex: 1, height: 40 }}
+                        titleStyle={styles.buttonTitle}
+                        style={styles.button}
                         onPress={this.props.handleDingPress}
                     />
                     <Image source={{uri: 'cell-button-line'}} style={{ width: 1, height: 29 }} />
                     <Button
                         leftImage='mainCellCai'
                         highlightLeftImage='mainCellCaiClick'
-                        leftImageStyle={{ width: 20, height: 20 }}
+                        leftImageStyle={styles.buttonLeftImage}
                         title={this.props.data.cai ? this.props.data.cai : '踩'}
                         highlightTitleColor='red'
-                        titleStyle={{ fontSize: 14, marginLeft: 5 }}
-                        style={{ flex: 1, height: 40 }}
+                        titleStyle={styles.buttonTitle}
+                        style={styles.button}
                         onPress={this.props.handleCaiPress}
                     />
                     <Image source={{uri: 'cell-button-line'}} style={{ width: 1, height: 29 }} />
                     <Button
                         leftImage='mainCellShare'
                         highlightLeftImage='mainCellShareClick'
-                        leftImageStyle={{ width: 20, height: 20 }}
+                        leftImageStyle={styles.buttonLeftImage}
                         title={this.props.data.repost ? this.props.data.repost : '分享'}
                         highlightTitleColor='red'
-                        titleStyle={{ fontSize: 14, marginLeft: 5 }}
-                        style={{ flex: 1, height: 40 }}
+                        titleStyle={styles.buttonTitle}
+                        style={styles.button}
                         onPress={this.props.handleSharePress}
                     />
                     <Image source={{uri: 'cell-button-line'}} style={{ width: 1, height: 29 }} />
                     <Button
                         leftImage='mainCellComment'
                         highlightLeftImage='mainCellCommentClick'
-                        leftImageStyle={{ width: 20, height: 20 }}
+                        leftImageStyle={styles.buttonLeftImage}
                         title={this.props.data.comment ? this.props.data.comment : '评论'}
                         highlightTitleColor='red'
-                        titleStyle={{ fontSize: 14, marginLeft: 5 }}
-                        style={{ flex: 1, height: 40 }}
+                        titleStyle={styles.buttonTitle}
+                        style={styles.button}
                         onPress={this.props.handleCommentPress}
                     />
                 </View>
@@ -62,5 +62,31 @@ class CellBottomView extends Component {
         );
     }
 }
+
+const styles = StyleSheet.create({
+    cellBottomViewLayout: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    bottomViewLayout: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: 40,
+    },
+    buttonLeftImage: {
+        width: 20,
+        height: 20,
+    },
+    buttonTitle: {
+        fontSize: 14,
+        marginLeft: 5,
+    },
+    button: {
+        flex: 1,
+        height: 40,
+    }
+});
 
 export default CellBottomView;
