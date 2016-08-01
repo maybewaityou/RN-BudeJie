64e5e4cf1675cea6b0b39c02a2b36bf398f59645
MeePwn <maybewaityou@gmail.com>
2016-08-01 16:59:07 +0800

    delete styles

--- app/main/tab/common/login/FastLoginButton.js
+++ app/main/tab/common/login/FastLoginButton.js
@@ -1,10 +1,10 @@
 /* jshint esversion: 6 */
 
 import React, { Component } from 'react';
-import styles from '../../../../styles/Main';
 import Images from '../../../constant/Images';
 import Dimensions from 'Dimensions';
 import {
+    StyleSheet,
     View,
     Text,
     Image,
@@ -26,7 +26,7 @@ class FastLoginButton extends React.Component {
         return (
             <TouchableOpacity
                 activeOpacity={1}
-                style={{marginTop: 20}}
+                style={styles.button}
                 onPress={this.props.onPress}
                 onPressIn={() => {
                     this.setState({image: this.props.clickedImage});
@@ -35,11 +35,28 @@ class FastLoginButton extends React.Component {
                     this.setState({image: this.props.image});
                 }}
             >
-                <Image source={{uri: this.state.image}} style={{ width: 70, height: 70 }} />
-                <Text style={[styles.commonText, { fontSize: 16, color: 'white', backgroundColor: '#00000000', marginTop: 10 }]}>{this.props.title}</Text>
+                <Image source={{uri: this.state.image}} style={styles.image} />
+                <Text style={styles.title}>{this.props.title}</Text>
             </TouchableOpacity>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    button: {
+        marginTop: 20,
+    },
+    image: {
+        width: 70,
+        height: 70,
+    },
+    title: {
+        fontSize: 16,
+        textAlign: 'center',
+        color: 'white',
+        backgroundColor: '#0000',
+        marginTop: 10,
+    },
+});
 
 export default FastLoginButton;
