ab5b90359b3d73c6e27bef50ed0ecaf1bde5eccf
MeePwn <maybewaityou@gmail.com>
2016-08-02 09:44:35 +0800

    add CellHotCommentView

--- app/main/tab/common/cell/CellHotCommentView.js
+++ app/main/tab/common/cell/CellHotCommentView.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import Images from '../../../constant/Images';
 import Button from '../../../components/Button';
 import {
+    StyleSheet,
     View,
     Text,
     Image
@@ -9,12 +10,34 @@ import {
 
 class CellHotCommentView extends Component {
     render() {
+        if (this.props.data.top_cmt.length === 0) {
+            return (<View />);
+        }
         return (
-            <View style={this.props.style}>
-
+            <View style={[styles.hotView, this.props.style]}>
+                <Text style={styles.commentDes}>最新评论</Text>
+                <Text style={styles.comment}>
+                    {this.props.data.top_cmt[0].user.username + ' : ' + this.props.data.top_cmt[0].content}
+                </Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    hotView: {
+        justifyContent: 'center',
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    commentDes: {
+        fontSize: 16,
+    },
+    comment: {
+        fontSize: 16,
+        marginTop: 5,
+    }
+});
 
 export default CellHotCommentView;
